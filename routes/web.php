3c98a14b9a1deedea108bc9a6dfb0e1de41394e2
Dyinastie Marchelina Puspitawening <dyinastiemp@gmail.com>
2024-02-23 10:48:17 +0700

    Jobsheet 2 : Modifikasi Praktikum Routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,45 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* Jobsheet 2 - Praktikum 1*/
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function () {
+    return 'NIM : 2241720166 | Nama : Dyinastie Marchelina Puspitawening';
+});
+
+/* Jobsheet 2 - Praktikum 2*/
+
+Route::get('user/{Dyinastie}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/post/{post1}/comments/{comment5}', function ($postId, $commentId) {
+    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
+});
+
+Route::get('/articles/{article4}', function ($id) {
+    return 'Halaman artikel dengan ID ' . $id;
+});
+
+/* Jobsheet 2 - Praktikum 3*/
+
+Route::get('/user/{name?}', function ($name = null) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/user/{name?}', function ($name = 'John') {
+    return 'Nama saya ' . $name;
+});
