ea9fe9858cc2c911cce30f12f04029a74a189c0d
Dyinastie Marchelina Puspitawening <dyinastiemp@gmail.com>
2024-03-22 11:14:23 +0700

    Jobsheet 6 : A. Template Form

--- routes/web.php
+++ routes/web.php
@@ -32,102 +32,102 @@ Route::get('/', function () {
 //     return 'Hello World';
 // });
 
-Route::get('/world', function () {
-    return 'World';
-});
-
-// Route::get('/', function () {
-//     return 'Selamat Datang';
+// Route::get('/world', function () {
+//     return 'World';
 // });
 
-Route::get('/about', function () {
-    return 'NIM : 2241720166 | Nama : Dyinastie Marchelina Puspitawening';
-});
+// // Route::get('/', function () {
+// //     return 'Selamat Datang';
+// // });
 
-/*-----------------------------------------------------------------------------*/
-/* Jobsheet 2 - Praktikum 2*/
-/*-----------------------------------------------------------------------------*/
-
-Route::get('user/{Dyinastie}', function ($name) {
-    return 'Nama saya ' . $name;
-});
-
-Route::get('/post/{post1}/comments/{comment5}', function ($postId, $commentId) {
-    return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
-});
-
-// Route::get('/articles/{article4}', function ($id) {
-//     return 'Halaman artikel dengan ID ' . $id;
+// Route::get('/about', function () {
+//     return 'NIM : 2241720166 | Nama : Dyinastie Marchelina Puspitawening';
 // });
 
-/*-----------------------------------------------------------------------------*/
-/* Jobsheet 2 - Praktikum 3*/
-/*-----------------------------------------------------------------------------*/
+// /*-----------------------------------------------------------------------------*/
+// /* Jobsheet 2 - Praktikum 2*/
+// /*-----------------------------------------------------------------------------*/
 
-Route::get('/user/{name?}', function ($name = null) {
-    return 'Nama saya ' . $name;
-});
-
-Route::get('/user/{name?}', function ($name = 'John') {
-    return 'Nama saya ' . $name;
-});
-
-
-/*=============================================================================*/
-/*-----------------------------------------------------------------------------*/
-/* Jobsheet 2 - Controller : Praktikum 1*/
-/*-----------------------------------------------------------------------------*/
-
-// Route::get('/hello', [WelcomeController::class, 'hello']);
-
-// Route::get('/', [PageController::class, 'index']);
-
-// Route::get('/about', [PageController::class, 'about']);
-
-// Route::get('/articles/{id}', [PageController::class, 'articles']);
-
-Route::get('/', [HomeController::class, 'index']);
-
-Route::get('/about', [AboutController::class, 'about']);
-
-Route::get('/articles/{id}', [ArticleController::class, 'articles']);
-
-/*-----------------------------------------------------------------------------*/
-/* Jobsheet 2 - Controller : Praktikum 2*/
-/*-----------------------------------------------------------------------------*/
-
-Route::resource('photos', PhotoController::class);
-
-Route::resource('photos', PhotoController::class)->only([
-    'index', 'show'
-]);
-Route::resource('photos', PhotoController::class)->except([
-    'create', 'store', 'update', 'destroy'
-]);
-
-
-/*=============================================================================*/
-/*-----------------------------------------------------------------------------*/
-/* Jobsheet 2 - Views : Praktikum 1*/
-/*-----------------------------------------------------------------------------*/
-
-// Route::get('/greeting', function () {
-//     return view('hello', ['name' => 'Dyinastie']);
+// Route::get('user/{Dyinastie}', function ($name) {
+//     return 'Nama saya ' . $name;
 // });
 
-/*-----------------------------------------------------------------------------*/
-/* Jobsheet 2 - Views : Praktikum 2*/
-/*-----------------------------------------------------------------------------*/
-
-// Route::get('/greeting', function () {
-//     return view('blog.hello', ['name' => 'Dyinastie']);
+// Route::get('/post/{post1}/comments/{comment5}', function ($postId, $commentId) {
+//     return 'Pos ke-' . $postId . " Komentar ke-: " . $commentId;
 // });
 
-/*-----------------------------------------------------------------------------*/
-/* Jobsheet 2 - Views : Praktikum 3*/
-/*-----------------------------------------------------------------------------*/
+// // Route::get('/articles/{article4}', function ($id) {
+// //     return 'Halaman artikel dengan ID ' . $id;
+// // });
 
-Route::get('/greeting', [
-    WelcomeController::class,
-    'greeting'
-]);
+// /*-----------------------------------------------------------------------------*/
+// /* Jobsheet 2 - Praktikum 3*/
+// /*-----------------------------------------------------------------------------*/
+
+// Route::get('/user/{name?}', function ($name = null) {
+//     return 'Nama saya ' . $name;
+// });
+
+// Route::get('/user/{name?}', function ($name = 'John') {
+//     return 'Nama saya ' . $name;
+// });
+
+
+// /*=============================================================================*/
+// /*-----------------------------------------------------------------------------*/
+// /* Jobsheet 2 - Controller : Praktikum 1*/
+// /*-----------------------------------------------------------------------------*/
+
+// // Route::get('/hello', [WelcomeController::class, 'hello']);
+
+// // Route::get('/', [PageController::class, 'index']);
+
+// // Route::get('/about', [PageController::class, 'about']);
+
+// // Route::get('/articles/{id}', [PageController::class, 'articles']);
+
+// Route::get('/', [HomeController::class, 'index']);
+
+// Route::get('/about', [AboutController::class, 'about']);
+
+// Route::get('/articles/{id}', [ArticleController::class, 'articles']);
+
+// /*-----------------------------------------------------------------------------*/
+// /* Jobsheet 2 - Controller : Praktikum 2*/
+// /*-----------------------------------------------------------------------------*/
+
+// Route::resource('photos', PhotoController::class);
+
+// Route::resource('photos', PhotoController::class)->only([
+//     'index', 'show'
+// ]);
+// Route::resource('photos', PhotoController::class)->except([
+//     'create', 'store', 'update', 'destroy'
+// ]);
+
+
+// /*=============================================================================*/
+// /*-----------------------------------------------------------------------------*/
+// /* Jobsheet 2 - Views : Praktikum 1*/
+// /*-----------------------------------------------------------------------------*/
+
+// // Route::get('/greeting', function () {
+// //     return view('hello', ['name' => 'Dyinastie']);
+// // });
+
+// /*-----------------------------------------------------------------------------*/
+// /* Jobsheet 2 - Views : Praktikum 2*/
+// /*-----------------------------------------------------------------------------*/
+
+// // Route::get('/greeting', function () {
+// //     return view('blog.hello', ['name' => 'Dyinastie']);
+// // });
+
+// /*-----------------------------------------------------------------------------*/
+// /* Jobsheet 2 - Views : Praktikum 3*/
+// /*-----------------------------------------------------------------------------*/
+
+// Route::get('/greeting', [
+//     WelcomeController::class,
+//     'greeting'
+// ]);
